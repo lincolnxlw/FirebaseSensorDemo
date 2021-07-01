a843e581f11990e0553fb60964e7a9e8867ce317
lincolnxlw <lincolnxlw@gmail.com>
2021-06-30 23:12:13 -0500

    Play around with a bunch of demos

---
+++ include/params.h
@@ -0,0 +1,14 @@
+#ifndef PARAMS_H_
+#define PARAMS_H_
+
+#define WIFI_SSID "AuburnWeb-Sec"
+#define WIFI_PASSWORD "AuburnWeb-Sec"
+
+#define API_KEY "AIzaSyDSTQfvFsBbxiB-NvqNT2m_469F7_6MA9k"
+#define DATABASE_URL "https://fir-b212e-default-rtdb.firebaseio.com"
+#define DATABASE_SECRET "zBzJJkC5ptMLg68dGW5dcY7v9eo8utPgFWzcIQuw"
+
+#define USER_EMAIL "lincolnxlw@gmail.com"
+#define USER_PASSWORD "Wxl910624gzcc"
+
+#endif   // PARAMS_H_
